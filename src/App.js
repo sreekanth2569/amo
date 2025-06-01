8ad7d59b433788affdbc40f7661758510f16559b
sreekanth avuleti <sreekanthavuleti@gmail.com>
2025-06-01 16:15:11 +0530

    UPDATED VULETI

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import Analytics from './components/Analytics';
 import Settings from './components/Settings';
 import JobsList from './components/JobsList';
 import JobDetail from './components/JobDetail';
+import Avuleti from './components/Avuleti';
 
 function App() {
   return (
     <div>
       <nav style={{ marginBottom: '20px' }}>
         <Link to='/'>Home</Link> | <Link to='/about'>About</Link> |{' '}
-        <Link to='/dashboard'>Dashboard</Link> | <Link to='/jobs'>Jobs</Link>
+        <Link to='/dashboard'>Dashboard</Link> | <Link to='/jobs'>Jobs</Link> |{' '}
+        <Link to='/avuleti'>AvuletI</Link>
       </nav>
 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/avuleti' element={<Avuleti />} />
 
         {/* Dashboard with nested routes */}
         <Route path='/dashboard' element={<Dashboard />}>
---
+++ src/components/Avuleti.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Avuleti = () => {
+  return (
+    <dih1v>
+      <h1>avuleti</h1>
+    </dih1v>
+  );
+};
+
+export default Avuleti;
