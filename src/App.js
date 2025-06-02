0c02342dc562934012214920d270d0fceba50aa6
sreekanth avuleti <sreekanthavuleti@gmail.com>
2025-06-02 08:12:01 +0530

    lol

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Settings from './components/Settings';
 import JobsList from './components/JobsList';
 import JobDetail from './components/JobDetail';
 import Avuleti from './components/Avuleti';
+import Emi from './components/Emi';
 
 /* # You already did this, but if not:
 git branch -M main
@@ -18,16 +19,23 @@ git push -u origin main
 function App() {
   return (
     <div>
-      <nav style={{ marginBottom: '20px' }}>
-        <Link to='/'>Home</Link> | <Link to='/about'>About</Link> |{' '}
-        <Link to='/dashboard'>Dashboard</Link> | <Link to='/jobs'>Jobs</Link> |{' '}
-        <Link to='/avuleti'>AvuletI</Link>
+      <nav
+        style={{
+          marginBottom: '20px',
+          border: '2px solid black',
+          borderRadius: '',
+        }}
+      >
+        <Link to='/'>Home</Link> | <Link to='/aboupx solid blackt'>About</Link>{' '}
+        | <Link to='/dashboard'>Dashboard</Link> | <Link to='/jobs'>Jobs</Link>{' '}
+        | <Link to='/avuleti'>AvuletI</Link> <Link to='/emi'> EMI</Link>
       </nav>
 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/avuleti' element={<Avuleti />} />
+        <Route path='/emi' element={<Emi />} />
 
         {/* Dashboard with nested routes */}
         <Route path='/dashboard' element={<Dashboard />}>
---
+++ src/components/Emi.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Emi = () => {
+  return (
+    <div>
+      <h1>i am emi</h1>
+    </div>
+  );
+};
+
+export default Emi;
